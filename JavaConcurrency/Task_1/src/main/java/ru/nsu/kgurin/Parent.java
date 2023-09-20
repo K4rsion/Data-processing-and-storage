86abf62877ff1c71d9ce0f9038454dfd5b11baac
Your Name <you@example.com>
2023-09-20 17:02:05 +0700

    conventions

--- JavaConcurrency/Task_1/src/main/java/ru/nsu/kgurin/Parent.java
+++ JavaConcurrency/Task_1/src/main/java/ru/nsu/kgurin/Parent.java
@@ -1,12 +1,12 @@
-package kgurin.nsu;
+package ru.nsu.kgurin;
 
 public class Parent {
-    static final int numberOfStringsToPrint = 10;
+    static final int NUMBER_OF_STRING_TO_PRINT = 10;
     public static void main(String[] args) {
-        Child child = new Child();
+        Child child = new Child(NUMBER_OF_STRING_TO_PRINT);
         Thread thread = new Thread(child);
         thread.start();
-        for (int i = 0; i < numberOfStringsToPrint; i++) {
+        for (int i = 0; i < NUMBER_OF_STRING_TO_PRINT; i++) {
             System.out.println("Hello from parent. Counter: " + (i + 1));
         }
     }
