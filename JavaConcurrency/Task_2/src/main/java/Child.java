dd495d3136ab9acd9f5946b92472a997f5af9276
Your Name <you@example.com>
2023-09-22 11:48:42 +0700

    task_2

--- JavaConcurrency/Task_2/src/main/java/Child.java
+++
@@ -1,9 +0,0 @@
-public class Child implements Runnable {
-
-    @Override
-    public void run() {
-        for (int i = 0; i < Parent.numberOfStringsToPrint; i++) {
-            System.out.println("Hello from Child. Counter: " + (i+1));
-        }
-    }
-}
