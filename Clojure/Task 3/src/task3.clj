27e92afa73f13feedc1752362267349dcbc9ed2d
Your Name <you@example.com>
2023-12-28 19:19:41 +0700

    Optimization

--- Clojure/Task 3/src/task3.clj
+++ Clojure/Task 3/src/task3.clj
@@ -3,12 +3,12 @@
 (defn parallel-filter
   ([pred coll]
    (let [n (. (Runtime/getRuntime) availableProcessors)
-         chunk-size 20
+         chunk-size 200
          chunk (map doall (partition-all chunk-size coll))
          future-filter #(future (doall (filter pred %)))
          pool (map future-filter chunk)
          step (fn step [cur fut]
                 (if-let [s (seq fut)]
-                  (lazy-seq (lazy-cat (deref (first cur)) (step (rest cur) (rest s))))
-                  (apply concat (map deref cur))))]
+                  (lazy-cat (deref (first cur)) (step (rest cur) (rest s)))
+                  (mapcat deref cur)))]
      (step pool (drop n pool)))))
--- Clojure/Task 3/src/task3_tests.clj
+++ Clojure/Task 3/src/task3_tests.clj
@@ -1,4 +1,4 @@
-(ns task3_tests
+(ns task3_test
   (:require [clojure.test :refer :all])
   (:use task3))
 
@@ -7,6 +7,8 @@
   (is (= (filter even? (range 100)) (parallel-filter even? (range 100))))
   (is (= (filter even? (range 1000)) (parallel-filter even? (range 1000)))))
 
+(deftest filter-time-test
+  (time (doall (take 1000 (filter even? (iterate inc 0))))))
+
 (deftest parallel-filter-time-test
-  (time (doall (take 100 (filter even? (iterate inc 0)))))
-  (time (doall (take 100 (parallel-filter even? (iterate inc 0))))))
+  (time (doall (take 1000 (parallel-filter even? (iterate inc 0))))))
