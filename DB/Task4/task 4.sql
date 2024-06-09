7375651c06fbbf3c91c66eb2d0783bc328799413
Your Name <you@example.com>
2024-06-10 00:34:22 +0700

    Added day and class

--- DB/Task4/task 4.sql
+++ DB/Task4/task 4.sql
@@ -1,14 +1,9 @@
-select flights.flight_no as flight, round(avg(ticket_flights.amount), 2) as price
-from flights
-left join ticket_flights
-on flights.flight_id = ticket_flights.flight_id 
-group by flights.flight_no
-order by flights.flight_no asc
-
--- SELECT flights.flight_no, ticket_flights.amount FROM ticket_flights
--- full join flights
--- on flights.flight_id = ticket_flights.flight_id
--- WHERE flight_no = 'PG0234';
-
--- select * from ticket_flights
--- where flight_id = 1;
+select 
+	f.flight_no as "race", 
+	tf.fare_conditions as "class", 
+	to_char(f.scheduled_departure::timestamp with time zone, 'Day') as "day", 
+	tf.amount as "price"
+from flights f
+inner join ticket_flights tf
+on f.flight_id = tf.flight_id
+order by tf.amount
